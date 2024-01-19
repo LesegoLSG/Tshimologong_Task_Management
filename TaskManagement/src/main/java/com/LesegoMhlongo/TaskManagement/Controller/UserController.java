3bc061aeab91aa8119cda6381fc7bed41bf73e67
LesegoLSG <217057620@student.uj.ac.za>
2024-01-19 09:43:57 -0600

    Testing an end point

--- TaskManagement/src/main/java/com/LesegoMhlongo/TaskManagement/Controller/UserController.java
+++ TaskManagement/src/main/java/com/LesegoMhlongo/TaskManagement/Controller/UserController.java
@@ -2,6 +2,7 @@ package com.LesegoMhlongo.TaskManagement.Controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/user")
+@CrossOrigin
 public class UserController {
 
-    @GetMapping
+    @GetMapping("/testing")
     public ResponseEntity<String> sayHello(){
         return ResponseEntity.ok("Hi user");
     }
