ec257cb534b80db5ed9ea9cd75cb89303543855a
LesegoLSG <217057620@student.uj.ac.za>
2024-01-19 09:41:31 -0600

    Main Page for authentication

--- React-task/react-task-management/src/Pages/AuthenticationPage.tsx
+++ React-task/react-task-management/src/Pages/AuthenticationPage.tsx
@@ -18,7 +18,7 @@ const AuthenticationPage: React.FC = () => {
             setChangeBtnName("In");
         }
     }
-
+    //Signup or register method, to be moved to authService file
     const signUp = async (userObject: any) => {
         console.log("Register button Clicked", userObject);
         try {
@@ -31,7 +31,7 @@ const AuthenticationPage: React.FC = () => {
             if (res.ok) {
                 const data = await res.json();
                 //setTasks((prevTasks) => [...prevTasks, data])
-                console.log("New Task Added");
+                console.log("New user Added");
             } else {
                 console.error("Failed to add task");
             }
@@ -41,13 +41,43 @@ const AuthenticationPage: React.FC = () => {
 
     }
 
+    //Signin or login method, to be moved to authService file
+    const signIn = async (loginCredentials: any) => {
+        console.log("Login credentials in signIn: ", loginCredentials);
+
+        try {
+            const response = await fetch("http://localhost:8080/api/v1/auth/signin", {
+                method: "POST",
+                headers: {
+                    "content-Type": "application/json",
+                },
+                body: JSON.stringify(loginCredentials),
+            });
+
+            if (response.ok) {
+                //receiving access token and refresh token (Same name conversion as backend)
+                const { token, refreshToken } = await response.json();
+                console.log("token:", token);
+                console.log("refreshToken", refreshToken);
+
+                //Testing token when request is called
+
+            } else {
+                throw new Error("Aunthentication failed");
+            }
+        } catch (error) {
+            console.error("Authentication failed:", error);
+            throw error;
+        }
+    }
+
     return (
         <div className="bg-gray-400 w-full h-screen flex justify-center items-center">
             <div className="bg-gray-800 w-[32rem] h-[40rem] flex justify-center items-center flex-col">
                 <button onClick={Switchscreen}
                     className="bg-red-300 text-white">sign {changeBtnName}</button>
                 {register ?
-                    <Login />
+                    <Login signIn={signIn} />
                     :
                     <Register setRegister={setRegister} signUp={signUp} />
                 }
