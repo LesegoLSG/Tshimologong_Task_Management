1beeecc626475507b3887e0810251e2312371b86
LesegoLSG <217057620@student.uj.ac.za>
2024-01-19 09:43:15 -0600

    Testing end point

--- TaskManagement/src/main/java/com/LesegoMhlongo/TaskManagement/Controller/AdminController.java
+++ TaskManagement/src/main/java/com/LesegoMhlongo/TaskManagement/Controller/AdminController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/admin")
 public class AdminController {
-    @GetMapping
+    @GetMapping("/getAdmin")
     public ResponseEntity<String> sayHello(){
         return ResponseEntity.ok("Hi admin");
     }
