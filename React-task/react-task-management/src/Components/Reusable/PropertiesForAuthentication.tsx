fcd4483496c7150bab66785c2b5dea9d2544a240
LesegoLSG <217057620@student.uj.ac.za>
2024-01-19 09:40:58 -0600

    Props for authentication

--- React-task/react-task-management/src/Components/Reusable/PropertiesForAuthentication.tsx
+++ React-task/react-task-management/src/Components/Reusable/PropertiesForAuthentication.tsx
@@ -2,3 +2,7 @@ export interface RegisterProps {
     setRegister: React.Dispatch<React.SetStateAction<boolean>>;
     signUp: (userObject: any) => Promise<void>;
 }
+
+export interface LoginProps {
+    signIn: (loginObject: any) => Promise<void>;
+}
