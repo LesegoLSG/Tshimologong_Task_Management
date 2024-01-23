e1e96172b691168427c7e5fc535dcb3edd29418f
LesegoLSG <217057620@student.uj.ac.za>
2024-01-22 23:14:53 -0600

    Added an endpoint for finding the user using an email

--- TaskManagement/src/main/java/com/LesegoMhlongo/TaskManagement/Controller/UserController.java
+++ TaskManagement/src/main/java/com/LesegoMhlongo/TaskManagement/Controller/UserController.java
@@ -1,20 +1,41 @@
 package com.LesegoMhlongo.TaskManagement.Controller;
 
+import com.LesegoMhlongo.TaskManagement.Model.UserEntity;
+import com.LesegoMhlongo.TaskManagement.Service.Impl.AuthenticationServiceImpl;
+import com.LesegoMhlongo.TaskManagement.dto.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/user")
-@CrossOrigin
 public class UserController {
+
+    @Autowired
+    private AuthenticationServiceImpl authenticationService;
+
+
 
     @GetMapping("/testing")
     public ResponseEntity<String> sayHello(){
         return ResponseEntity.ok("Hi user");
+    }
+
+    @GetMapping("/getAll")
+    public List<UserEntity> getAllUsers(){
+        System.out.println(authenticationService.findAllUsers());
+        return authenticationService.findAllUsers();
+    }
+
+    @GetMapping("/findUserByEmail/{email}")
+    public ResponseEntity<CustomUserDetails> getUser(@PathVariable String email){
+        CustomUserDetails userDetails = authenticationService.findUserByEmail(email);
+        return ResponseEntity.ok(userDetails);
     }
 }
