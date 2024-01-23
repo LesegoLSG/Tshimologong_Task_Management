d15e2225f84f032cf66f6ff381e60311ce1cba8c
LesegoLSG <217057620@student.uj.ac.za>
2024-01-22 23:10:15 -0600

    Added useContext import(currently not used)

--- React-task/react-task-management/src/index.tsx
+++ React-task/react-task-management/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { UserProvider } from '../src/Components/Context/UserContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
