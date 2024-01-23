dda0c1a94aeecf3bdf6dab8ceecdf492d1ab63f0
LesegoLSG <217057620@student.uj.ac.za>
2024-01-22 23:01:28 -0600

    Testing navbar

--- React-task/react-task-management/src/App.tsx
+++ React-task/react-task-management/src/App.tsx
@@ -5,6 +5,8 @@ import MainPage from './Pages/Home';
 import AuthenticationPage from './Pages/AuthenticationPage';
 import Register from './Components/LoginAndRegister/Register';
 import Login from './Components/LoginAndRegister/Login';
+import HomePage from './Pages/HomePage';
+import NavBar from './Components/NavigationBar/NavBar';
 
 
 function App() {
@@ -13,8 +15,12 @@ function App() {
     <div className="App">
 
       <Router>
+        {/* <NavBar /> */}
         <Routes>
-          <Route path="/" element={<AuthenticationPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<AuthenticationPage />} />
+          {/* <Route path="/register" element={<} */}
+
           <Route path="/main" element={<MainPage />} />
           <Route path="*" element={<h1>Page Not Found</h1>} />
 
