eea25e87cb48f766e391e4e44acb3e6f1d7612ff
LesegoLSG <217057620@student.uj.ac.za>
2024-01-22 23:05:07 -0600

    Added a function to get the current user my email from the token

--- React-task/react-task-management/src/Components/RequestsToken/AuthService.tsx
+++ React-task/react-task-management/src/Components/RequestsToken/AuthService.tsx
@@ -1,13 +1,66 @@
-import axios from 'axios';
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-interface APIProps {
-    token: string;
+
+const API_URL = "http://localhost:8080/api/v1/auth";
+
+export const signUp = async (userObject: any) => {
+    return axios.post(API_URL + "/signup", userObject)
+        .then((response) => {
+            // localStorage.setItem("user", JSON.stringify(response.data));
+            return response.data;
+        })
+        .catch((error) => {
+            console.error("Error in signUp:", error);
+            throw error;
+        });
+};
+
+export const signIn = async (userObject: any) => {
+
+    return axios.post(API_URL + "/signin", userObject)
+        .then((response) => {
+
+            // if (response.data.accessToken) {
+            //     console.log("accessToken:", response.data.accessToken);
+            //     console.log("accessToken:", response.data.refreshToken);
+
+            // }
+            localStorage.setItem("user", JSON.stringify(response.data));
+            console.log("Data:", response.data);
+            return response.data;
+
+
+        })
+        .catch((error) => {
+            console.error("Error in signIn:", error);
+            throw error;
+        });
+};
+
+export const logout = () => {
+    localStorage.removeItem("user");
 }
 
-const APIUrl = 'http:localhost:8080/api/v1/auth';
+export const getCurrentUserEmail = () => {
+    try {
+        const token = localStorage.getItem("user");
+        if (!token) return null;
+        console.log("Auth token:", token)
 
-let accessToken = null;
+        const payload = token.split('.')[1];
+        const decodedPayload = atob(payload);
+        const parsePayload = JSON.parse(decodedPayload);
 
-const setAccessToken = (token: APIProps) => {
-    accessToken = token;
+        const email = parsePayload.sub;
+        console.log("AuthService email:", email);
+        return email;
+
+    } catch (error) {
+        console.log("UserByEmail error:", error);
+    }
+
+
+
 }
+
