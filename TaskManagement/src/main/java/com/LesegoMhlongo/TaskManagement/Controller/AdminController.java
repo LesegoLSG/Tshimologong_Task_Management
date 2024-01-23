229f263ffc8375dfd72c23dacfc9b29eb85b771a
LesegoLSG <217057620@student.uj.ac.za>
2024-01-22 23:13:15 -0600

    Testing the endpoint

--- TaskManagement/src/main/java/com/LesegoMhlongo/TaskManagement/Controller/AdminController.java
+++ TaskManagement/src/main/java/com/LesegoMhlongo/TaskManagement/Controller/AdminController.java
@@ -2,10 +2,11 @@ package com.LesegoMhlongo.TaskManagement.Controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/admin")
