811d7fe8899256d50e13b443653055d3986fd549
LesegoLSG <217057620@student.uj.ac.za>
2024-02-04 23:52:27 -0600

    Added a method addTaskToUser

--- TaskManagement/src/main/java/com/LesegoMhlongo/TaskManagement/Controller/TaskController.java
+++ TaskManagement/src/main/java/com/LesegoMhlongo/TaskManagement/Controller/TaskController.java
@@ -1,47 +1,52 @@
 package com.LesegoMhlongo.TaskManagement.Controller;
 
 import com.LesegoMhlongo.TaskManagement.Model.Task;
+import com.LesegoMhlongo.TaskManagement.Model.UserEntity;
+import com.LesegoMhlongo.TaskManagement.Repository.TaskRepository;
+import com.LesegoMhlongo.TaskManagement.Repository.UserRepository;
 import com.LesegoMhlongo.TaskManagement.Service.ITaskService;
-import jakarta.persistence.Entity;
+import com.LesegoMhlongo.TaskManagement.Service.Impl.AuthenticationServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/tasks")
+@RequestMapping("api/v1/tasks")
 @CrossOrigin
 public class TaskController {
     @Autowired
-    private ITaskService service;
+    private TaskRepository taskRepository;
 
-    //Save a task using service
-    @PostMapping("/add")
-    public Task addNewTask(@RequestBody Task task){
+    @Autowired
+    private UserRepository userRepository;
 
-        return service.saveTask(task);
+    @PostMapping("/addTaskToUser/{userId}")
+    public ResponseEntity<UserEntity> addTaskToUser(@RequestBody Task task, @PathVariable int userId) {
+        // Find user by ID
+        Optional<UserEntity> optionalUser = userRepository.findById(userId);
+
+        if (optionalUser.isPresent()) {
+            UserEntity user = optionalUser.get();
+
+            // Set the user for the task
+            task.setUser(user);
+
+            // Save the task
+            taskRepository.save(task);
+
+            // Update the user's task list
+            user.getTask().add(task);
+
+            // Save the updated user
+            userRepository.save(user);
+
+            return ResponseEntity.ok(user);
+        } else {
+            // Handle the case where the user with the provided ID is not found
+            throw new IllegalArgumentException("User not found with ID: " + userId);
+        }
     }
-    //Get all tasks from service
-    @GetMapping("/getAllTask")
-    public List<Task> getAllTasks(){
-        return service.TaskList();
-    }
-    //Get a single task
-    @GetMapping("/getTask/{id}")
-    public Task getTask(@PathVariable int id){
-        return service.getSingleTask(id);
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public void deleteTask(@PathVariable int id){
-        service.deleteSingleTask(id);
-    }
-
-    @PutMapping("/update/{id}")
-    public void updateTask(@PathVariable int id, @RequestBody Task task){
-        service.updateSingleTask(id,task);
-    }
-
-
-
 }
