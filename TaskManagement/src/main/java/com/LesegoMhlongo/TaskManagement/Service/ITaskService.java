677e143a4457a03351fc07551f78e9bb42f95795
LesegoLSG <217057620@student.uj.ac.za>
2024-02-04 23:58:38 -0600

    Added an addTask method

--- TaskManagement/src/main/java/com/LesegoMhlongo/TaskManagement/Service/ITaskService.java
+++ TaskManagement/src/main/java/com/LesegoMhlongo/TaskManagement/Service/ITaskService.java
@@ -1,16 +1,12 @@
 package com.LesegoMhlongo.TaskManagement.Service;
 
 import com.LesegoMhlongo.TaskManagement.Model.Task;
+import com.LesegoMhlongo.TaskManagement.Model.UserEntity;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ITaskService {
-    public Task saveTask(Task task);
-    public List<Task> TaskList();
-
-    public Task getSingleTask(int id);
-
-    public void updateSingleTask(int id, Task updatedTask);
-    public void deleteSingleTask(int id);
+//    public UserEntity addTaskToUser(Task task, int userId);
+        public Task addTask(Task task);
 }
