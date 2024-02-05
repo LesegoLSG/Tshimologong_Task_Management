9b7e01a5df336823296a3854b02836316389517d
LesegoLSG <217057620@student.uj.ac.za>
2024-02-04 23:42:46 -0600

    Included a PostTaskToUser api request

--- React-task/react-task-management/src/Components/RequestsToken/PostService.tsx
+++ React-task/react-task-management/src/Components/RequestsToken/PostService.tsx
@@ -2,10 +2,11 @@ import axios, { AxiosRequestConfig } from "axios";
 import authHeader from "./AuthHeader";
 import { getCurrentUserEmail } from "./AuthService";
 import api from './RefreshTokenHandler';
+import { TaskProps } from "../Reusable/Properties";
 
 const API_URL = "http://localhost:8080/api/v1/auth"
 
-// const API_URL_private = "http://localhost:8080/api/v1/user"
+const API_URL_private = "http://localhost:8080/api/v1/user"
 
 const PostService = {
     getAllPublicPosts: async () => {
@@ -32,14 +33,27 @@ const PostService = {
 
     getCurrentUser: async () => {
         try {
-            const response = await api.get('/user/findUserByEmail/' + getCurrentUserEmail());
-            console.log('User custom:', response.data);
+            const email = getCurrentUserEmail();
+            // const headers = authHeader(); // Ensure headers is an object
+            // console.log("Headers:", headers);
+            const response = await api.get(`user/getUserWithTasks/${email}`);
+            console.log("User custom:", response.data);
             return response.data;
         } catch (error) {
-            console.error('Error in getCurrentUser:', error);
+            console.error("Error in getAllPrivatePosts:", error);
             throw error;
         }
     },
+
+    postTaskToUser: async (userId: any, task: TaskProps) => {
+        try {
+            const res = await api.post(`tasks/addTaskToUser/${userId}`, task)
+            console.log("res:PostTaskToUser:", res);
+            return res;
+        } catch (error) {
+            console.log(error);
+        }
+    }
 };
 
 export default PostService;
