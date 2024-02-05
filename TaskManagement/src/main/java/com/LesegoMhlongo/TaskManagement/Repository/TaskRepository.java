f3cf6161e2cc54661b846092636c8a5022c2ffd3
LesegoLSG <217057620@student.uj.ac.za>
2024-02-04 23:57:29 -0600

    Testing

--- TaskManagement/src/main/java/com/LesegoMhlongo/TaskManagement/Repository/TaskRepository.java
+++ TaskManagement/src/main/java/com/LesegoMhlongo/TaskManagement/Repository/TaskRepository.java
@@ -4,6 +4,10 @@ import com.LesegoMhlongo.TaskManagement.Model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task,Integer> {
+    //Custom query to find tasks by ID
+//    List<Task> findByUserId(int userId);
 }
